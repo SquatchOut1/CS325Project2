d0dffd269eadff71b89312b84e3a5dd2a960cbbc
alexwernex <alexwernex@illinois.edu>
2023-09-19 13:38:16 -0500

    Testing argparse

--- HTMLDownload.py
+++ HTMLDownload.py
@@ -1,3 +1,16 @@
 
 
-print("Test")
+############################
+# Alex Wernex              #
+# Cs325 Project 1          #
+############################
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(help="URL to download", dest="url", type=str)
+
+arg = parser.parse_args()
+url = arg.url
+
+print(url)
