2bf3149ce230f72456b7197b07c4cbf99ca4ace3
alexwernex <alexwernex@illinois.edu>
2023-09-19 13:55:07 -0500

    testing url download function

--- HTMLDownload.py
+++ HTMLDownload.py
@@ -6,6 +6,9 @@
 ############################
 
 import argparse
+import urllib.request
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(help="URL to download", dest="url", type=str)
@@ -14,3 +17,5 @@ arg = parser.parse_args()
 url = arg.url
 
 print(url)
+
+urllib.request.urlretrieve('https://www.reddit.com/r/mtg/comments/16msc6b/finished_a_game_at_130000_and_still_won_it/', "test.txt")
