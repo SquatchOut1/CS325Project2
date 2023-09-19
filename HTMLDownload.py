831fe28f0efc364c6d860fc81df6c4b0cf253ab3
alexwernex <alexwernex@illinois.edu>
2023-09-19 14:19:31 -0500

    combined argarse and downloader

--- HTMLDownload.py
+++ HTMLDownload.py
@@ -16,6 +16,4 @@ parser.add_argument(help="URL to download", dest="url", type=str)
 arg = parser.parse_args()
 url = arg.url
 
-print(url)
-
-urllib.request.urlretrieve('https://www.reddit.com/r/mtg/comments/16msc6b/finished_a_game_at_130000_and_still_won_it/', "test.txt")
+urllib.request.urlretrieve(url, "HTMLOutput.txt")
